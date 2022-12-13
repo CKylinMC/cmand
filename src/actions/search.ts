ddbc365bc184bb6dd4d7a05a1d82028579fef9c8
CKylinMC <canszj@yeah.net>
2022-12-13 10:48:35 +0800

    fix: search cmd

--- src/actions/search.ts
+++ src/actions/search.ts
@@ -4,11 +4,11 @@ import Db from "../lib/Db";
 export async function search(searchText) {
     const scripts = await Db.searchScripts(new RegExp(searchText,'ig'));
     if (scripts.length === 0) {
-        console.log(chalk.yellow('No scripts found.'));
+        console.log(chalk.yellow('No script match your input.'));
     } else {
         console.log('Scripts matched "'+searchText+'":');
         scripts.forEach(script => {
-            console.log(chalk[script.enabled ? 'green' : 'red'](script.reqAdmin?`[*]`:`[ ]`,`${script.name} - ${script.description} (${script.path})`));
+            console.log(chalk[script.enabled ? 'green' : 'red'](script.reqAdmin?`*`:' ',`${chalk.bold(script.name)}${script.description.length?chalk.magenta(' - '+script.description):''} (${chalk.gray(script.path)})`));
         });
     }
 }
