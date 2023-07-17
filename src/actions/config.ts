ab6119d8720db1812c9c0e9a96dc87e0dfe47c14
CKylinMC <canszj@yeah.net>
2023-07-17 12:00:06 +0800

    feat: prevent repos field changes by config. It should only be changed from repo command

--- src/actions/config.ts
+++ src/actions/config.ts
@@ -1,13 +1,24 @@
 import chalk from 'chalk';
 import { Settings } from "../lib/Db";
 
+function isBlocked(key) {
+    switch (key) {
+        case 'repos':
+            console.log(chalk.red('Please use "cmand repo" to manage repositories.'));
+            return true;
+    }
+    return false;
+}
+
 export async function setConfig(key, value) {
+    if(isBlocked(key)) return;
     Settings.set(key, value);
     console.log(chalk.green(`Config ${key} added.`));
     return;
 }
 
 export async function removeConfig(key) {
+    if(isBlocked(key)) return;
     Settings.remove(key);
     console.log(chalk.green(`Config ${key} removed.`));
     return;
@@ -23,6 +34,7 @@ export async function listConfig() {
 }
 
 export async function getConfig(key) {
+    if(isBlocked(key)) return;
     const value = await Settings.get(key);
     console.log(chalk.yellow(key),'=',chalk.green(value));
     return;
