82240404ab2aac5406d5f037955db813d8b42371
CKylinMC <canszj@yeah.net>
2023-12-01 17:50:07 +0800

    build: update deps not all updated: some of them not support current project structure anymore.

--- src/info.ts
+++ src/info.ts
@@ -3,7 +3,7 @@ import homedir from 'homedir';
 
 export const Info = {
     name: 'cmand',
-    version: '0.8.0-beta',
+    version: '0.8.1-beta',
     description: 'A command line tool for managing your script snippets on Windows.',
     author: 'CKylinMC',
 }
